4a4ca1e2cc5d0b63cf939bbfda5e12b8fd172da3
minhluu2608 <minhluu2608@gmail.com>
2022-08-18 20:35:13 +0700

    Cap nhat mobileAPI

---
+++ ServerAPI/Controllers/MobileAppController.cs
@@ -0,0 +1,195 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using System.Data;
+using ServerAPI.Models;
+
+namespace ServerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MobileAppController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public MobileAppController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet("{username}/{password}")]
+        public JsonResult CheckLogin(string username, string password)
+        {
+            string query = "Select * from account where username = '" + username + "' and password = '" + password + "'";
+            DataTable table = new DataTable();
+
+            SqlDataReader myReader;
+            string sqlDataSource = _configuration.GetConnectionString("DBCon");
+
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            if(table.Rows.Count > 0)
+            {
+                return new JsonResult(true);
+            }
+            else
+            {
+                return new JsonResult(false);
+            }
+            
+        }
+
+        [HttpGet("getIDAccount/{username}/{password}")]
+        public JsonResult getIDAccount(string username, string password)
+        {
+            string query = "Select IDAccount from account where username = '" + username + "' and password = '" + password + "'";
+            DataTable table = new DataTable();
+
+            SqlDataReader myReader;
+            string sqlDataSource = _configuration.GetConnectionString("DBCon");
+
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult(table.Rows[0][0].ToString());
+
+        }
+
+        [HttpGet("{username}/{password}/{SDT}")]
+        public JsonResult RegisterAccount(string username, string password, string sdt)
+        {
+            string queryCheckUsername = @"Select * from Account where username = '" + username + "'";
+            DataTable tblCheckUsername = new DataTable();
+            string queryCheckSDT = @"Select * from Account where SDT = '" + sdt + "'";
+            DataTable tblCheckSDT = new DataTable();
+
+            SqlDataReader myReader;
+            string sqlDataSource = _configuration.GetConnectionString("DBCon");
+
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+
+                using (SqlCommand myCommand = new SqlCommand(queryCheckUsername, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    tblCheckUsername.Load(myReader);
+                    myReader.Close();
+                }
+                if(tblCheckUsername.Rows.Count > 0)
+                {
+
+                    myCon.Close();
+                    return new JsonResult("Username đã tồn tại");
+                }
+
+                using (SqlCommand myCommand = new SqlCommand(queryCheckSDT, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    tblCheckSDT.Load(myReader);
+                    myReader.Close();
+                }
+                if(tblCheckSDT.Rows.Count > 0)
+                {
+
+                    myCon.Close();
+                    return new JsonResult("Số điện thoại đã tồn tại");
+                }
+
+                string queryGetIDAccount = "Select max(IDAccount) + 1 from Account";
+                DataTable tblIDAccount = new DataTable();
+                using (SqlCommand myCommand = new SqlCommand(queryGetIDAccount, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    tblIDAccount.Load(myReader);
+                    myReader.Close();
+                }
+                string IDAccount = tblIDAccount.Rows[0][0].ToString();
+                string queryInsertAccount = @"Insert into Account values 
+                    (" + IDAccount + ", '" + username + "', '" + password + "', '" + sdt +"', 'anonymous.png')";
+                using (SqlCommand myCommand = new SqlCommand(queryInsertAccount, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    myReader.Close();
+                }
+                myCon.Close();
+                return new JsonResult("Đăng ký thành công");
+            }
+        }
+
+        [HttpPost("link")]
+        public JsonResult PostLinkAccount(LinkedAccount linkAccount)
+        {
+            SqlDataReader myReader;
+            string sqlDataSource = _configuration.GetConnectionString("DBCon");
+
+            string queryGetIDKH = "Select IDKhachHang from KhachHang where MaKhachHang = '" + linkAccount.MaKH + "'";
+            DataTable tblIDKH = new DataTable();
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(queryGetIDKH, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    tblIDKH.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            if(tblIDKH.Rows.Count == 0)
+            {
+                return new JsonResult("Mã khách hàng không tồn tại");
+            }
+            string IDKhachHang = tblIDKH.Rows[0][0].ToString();
+
+            string queryCheckIsLinked = "Select * from LienKetTK where IDAccount = " + linkAccount.IDAccount + 
+                " and IDKhachHang = '" + IDKhachHang + "'";
+            DataTable tblCheckIsLinked = new DataTable();
+
+
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(queryCheckIsLinked, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    tblCheckIsLinked.Load(myReader);
+                    myReader.Close();
+                }
+                if(tblCheckIsLinked.Rows.Count > 0)
+                {
+                    myCon.Close();
+                    return new JsonResult("Đã liên kết với mã khách hàng này");
+                }
+
+                string queryInsert = "Insert into LienKetTK values(" + linkAccount.IDAccount + ", " + IDKhachHang + ")";
+                using (SqlCommand myCommand = new SqlCommand(queryInsert, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    myReader.Close();
+                }
+                myCon.Close();
+                return new JsonResult("Liên kết thành công");
+            }
+        }
+    }
+}
---
+++ ServerAPI/Models/LinkedAccount.cs
@@ -0,0 +1,9 @@
+﻿namespace ServerAPI.Models
+{
+    public class LinkedAccount
+    {
+        public int IDAccount { get; set; }
+        public string MaKH { get; set; }
+
+    }
+}
