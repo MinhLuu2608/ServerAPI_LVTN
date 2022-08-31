d32837e719d31fee73b54758c8a072f5cb32123d
minhluu2608 <minhluu2608@gmail.com>
2022-08-31 19:50:29 +0700

    cap nhat api donhang

--- ServerAPI/Controllers/DonHangController.cs
+++ ServerAPI/Controllers/DonHangController.cs
@@ -103,6 +103,42 @@ namespace ServerAPI.Controllers
             return new JsonResult(table);
         }
 
+        [HttpPost("received")]
+        public JsonResult PostReceivedDonHang(DonHang dh)
+        {
+            string selectString = @"Select *, convert(varchar, NgayTao, 103) as NgayTaoConvert,
+                convert(varchar, NgayHen, 103) as NgayHenConvert
+                from DonHangDV 
+                join ChiTietTiepNhanDonHang on DonHangDV.IDDonHang = ChiTietTiepNhanDonHang.IDDonHang
+                join NhanVien on NhanVien.IDNhanVien = ChiTietTiepNhanDonHang.IDNhanVien
+                join Account on Account.IDAccount = DonHangDV.IDAccount ";
+            string whereString = " where NhanVien.IDNhanVien = " + dh.IDNhanVien + 
+                " and TenKhachHang like N'%" + dh.TenKhachHang + "%' ";
+            string orderByString = " order by NgayHen";
+            if (dh.TinhTrangXuLy != "-1")
+            {
+                whereString = string.Concat(whereString, " and TinhTrangXuLy = N'" + dh.TinhTrangXuLy + "'");
+            }
+            string query = string.Concat(selectString, whereString, orderByString);
+            DataTable table = new DataTable();
+
+            SqlDataReader myReader;
+            string sqlDataSource = _configuration.GetConnectionString("DBCon");
+
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult(table);
+        }
+
         [HttpPost("accept")]
         public JsonResult PostAcceptDonHang(TiepNhanDH dh)
         {
@@ -149,7 +185,6 @@ namespace ServerAPI.Controllers
                 string queryUpdateDonHang = @"Update DonHangDV set NgayHen = '" + dh.NgayHen + @"', 
                     BuoiHen = N'" + dh.BuoiHen + @"', TinhTrangXuLy = N'Đã tiếp nhận' 
                     where IDDonHang = " + dh.IDDonhang;
-                Console.WriteLine(queryUpdateDonHang);
                 string queryInsertTiepNhanDH = @"Insert into ChiTietTiepNhanDonHang values(
                     " + dh.IDDonhang + ", " + dh.IDNhanVien + ")";
                 using (SqlCommand myCommand = new SqlCommand(queryUpdateDonHang, myCon))
@@ -167,6 +202,110 @@ namespace ServerAPI.Controllers
                 {
                     severity = "success",
                     message = "Tiếp nhận đơn hàng thành công."
+                });
+            }
+        }
+
+        [HttpPost("cancel")]
+        public JsonResult PostCancelDonHang(DonHang dh)
+        {
+
+            string checkQuery = @"Select * from DonHangDV where NgayHen is not null and IDDonhang = " + dh.IDDonHang;
+            DataTable tblCheck = new DataTable();
+
+            SqlDataReader myReader;
+            string sqlDataSource = _configuration.GetConnectionString("DBCon");
+
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(checkQuery, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    tblCheck.Load(myReader);
+                    myReader.Close();
+                }
+                if (tblCheck.Rows.Count > 0)
+                {
+                    myCon.Close();
+                    return new JsonResult(new
+                    {
+                        severity = "warning",
+                        message = "Đơn hàng này đã được tiếp nhận. Hãy kiểm tra lại"
+                    }
+                    );
+                }
+
+                string queryUpdateDonHang = @"Update DonHangDV set Note = N'" + dh.Note + @"',
+                    TinhTrangXuLy = N'Đã bị huỷ' where IDDonHang = " + dh.IDDonHang;
+                string queryInsertTiepNhanDH = @"Insert into ChiTietTiepNhanDonHang values(
+                    " + dh.IDDonHang + ", " + dh.IDNhanVien + ")";
+                using (SqlCommand myCommand = new SqlCommand(queryUpdateDonHang, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    myReader.Close();
+                }
+                using (SqlCommand myCommand = new SqlCommand(queryInsertTiepNhanDH, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    myReader.Close();
+                }
+                myCon.Close();
+                return new JsonResult(new
+                {
+                    severity = "success",
+                    message = "Huỷ đơn hàng thành công."
+                });
+            }
+        }
+
+        [HttpPost("cancelreceveid")]
+        public JsonResult PostCancelReceivedDonHang(DonHang dh)
+        {
+            SqlDataReader myReader;
+            string sqlDataSource = _configuration.GetConnectionString("DBCon");
+
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+
+                string queryUpdateDonHang = @"Update DonHangDV set Note = N'" + dh.Note + @"',
+                    TinhTrangXuLy = N'Đã bị huỷ' where IDDonHang = " + dh.IDDonHang;
+                using (SqlCommand myCommand = new SqlCommand(queryUpdateDonHang, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    myReader.Close();
+                }
+                myCon.Close();
+                return new JsonResult(new
+                {
+                    severity = "success",
+                    message = "Huỷ đơn hàng thành công."
+                });
+            }
+        }
+
+        [HttpPut("recovery")]
+        public JsonResult PutRecoveryDonHang(DonHang dh)
+        {
+            SqlDataReader myReader;
+            string sqlDataSource = _configuration.GetConnectionString("DBCon");
+
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                string queryUpdateDonHang = @"Update DonHangDV set Note = N'',
+                    TinhTrangXuLy = N'Đã tiếp nhận' where IDDonHang = " + dh.IDDonHang;
+                using (SqlCommand myCommand = new SqlCommand(queryUpdateDonHang, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    myReader.Close();
+                }
+                myCon.Close();
+                return new JsonResult(new
+                {
+                    severity = "success",
+                    message = "Phục hồi đơn hàng thành công."
                 });
             }
         }
--- ServerAPI/Models/DonHang.cs
+++ ServerAPI/Models/DonHang.cs
@@ -2,12 +2,13 @@
 {
     public class DonHang
     {
-        public int? IDDonhang { get; set; }
+        public int? IDDonHang { get; set; }
         public int? IDNhanVien { get; set; }
         public string? MaDonHang { get; set; }
         public string? TenKhachHang { get; set; }
         public DateTime? NgayHen { get; set; }
         public string? BuoiHen { get; set; }
+        public string? Note { get; set; }
         public string? TinhTrangXuLy { get; set; }
     }
 }
